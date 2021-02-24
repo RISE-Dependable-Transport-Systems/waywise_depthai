73cc3394bcba1d9fe1f80ba1df60ef940fc87973
Marvin Damschen <marvin.damschen@ri.se>
2021-02-24 13:58:39 +0100

    integrate birdview from depthai-experiments/social-distancing

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from time import sleep
 import depthai
 import cv2
 from PIL import Image
+import math
+import numpy as np
 
 
 class TCPServerRequest(socketserver.BaseRequestHandler):
@@ -48,6 +50,60 @@ class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     """Handle requests in a separate thread."""
     pass
 
+class BirdFrame():
+    max_z = 6
+    min_z = 0
+    max_x = 1.0
+    min_x = -1.0
+
+    def __init__(self):
+        self.distance_bird_frame = self.make_bird_frame()
+
+    def make_bird_frame(self):
+        fov = 68.7938
+        min_distance = 0.827
+        frame = np.zeros((320, 100, 3), np.uint8)
+        min_y = int((1 - (min_distance - self.min_z) / (self.max_z - self.min_z)) * frame.shape[0])
+        cv2.rectangle(frame, (0, min_y), (frame.shape[1], frame.shape[0]), (70, 70, 70), -1)
+
+        alpha = (180 - fov) / 2
+        center = int(frame.shape[1] / 2)
+        max_p = frame.shape[0] - int(math.tan(math.radians(alpha)) * center)
+        fov_cnt = np.array([
+            (0, frame.shape[0]),
+            (frame.shape[1], frame.shape[0]),
+            (frame.shape[1], max_p),
+            (center, frame.shape[0]),
+            (0, max_p),
+            (0, frame.shape[0]),
+        ])
+        cv2.fillPoly(frame, [fov_cnt], color=(70, 70, 70))
+        return frame
+
+    def calc_x(self, val):
+        norm = min(self.max_x, max(val, self.min_x))
+        center = (norm - self.min_x) / (self.max_x - self.min_x) * self.distance_bird_frame.shape[1]
+        bottom_x = max(center - 2, 0)
+        top_x = min(center + 2, self.distance_bird_frame.shape[1])
+        return int(bottom_x), int(top_x)
+
+    def calc_z(self, val):
+        norm = min(self.max_z, max(val, self.min_z))
+        center = (1 - (norm - self.min_z) / (self.max_z - self.min_z)) * self.distance_bird_frame.shape[0]
+        bottom_z = max(center - 2, 0)
+        top_z = min(center + 2, self.distance_bird_frame.shape[0])
+        return int(bottom_z), int(top_z)
+
+    def parse_frame(self, frame, detections):
+        bird_frame = self.distance_bird_frame.copy()
+
+        for detection in detections:
+            left, right = self.calc_x(detection.depth_x)
+            top, bottom = self.calc_z(detection.depth_z)
+            cv2.rectangle(bird_frame, (left, top), (right, bottom), (0, 255, 0), 2)
+
+        return np.hstack((frame, bird_frame))
+
 
 # start TCP data server
 server_TCP = socketserver.TCPServer(('192.168.222.3', 8070), TCPServerRequest)
@@ -79,6 +135,7 @@ if pipeline is None:
     raise RuntimeError("Error initializing pipelne")
 
 detections = []
+bf = BirdFrame()
 
 while True:
     nnet_packets, data_packets = pipeline.get_available_nnet_and_data_packets()
@@ -103,9 +160,10 @@ while True:
 
                 cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
+            annotated_frame = bf.parse_frame(frame, detections)
             server_TCP.datatosend = json.dumps([detection.get_dict() for detection in detections])
-            server_HTTP.frametosend = frame
-            cv2.imshow('previewout', frame)
+            server_HTTP.frametosend = annotated_frame
+            cv2.imshow('previewout', annotated_frame)
 
     if cv2.waitKey(1) == ord('q'):
         break
