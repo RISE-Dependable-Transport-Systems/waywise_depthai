9312919185d932b5e7407a1948adb35a4c6ad7aa
Rickard Häll <76961018+rickaha@users.noreply.github.com>
2022-09-14 10:13:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # Create virtualenv and install dependencies
 #!/bin/bash 
-python3 -m venv sdvp_depthaigen2
-source sdvp_depthaigen2/bin/activate
+python3 -m venv waywise_depthaigen2
+source waywise_depthaigen2/bin/activate
 python3 install_requirements.py
 deactivate
